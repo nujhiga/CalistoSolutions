55717aee1cca74c9adaaa7068a8b90c1c3bec4ca
joaquin cozzani <joacocozzani@hotmail.com>
2022-03-31 13:06:59 -0300

    TEST - Chaging Expressions Sealed Classes as Builders to Static Classes Factories

---
+++ CalistoDbCore/Expressions/Builders/BaseExpressionsBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CalistoDbCore.Expressions.Builders;
+
+public abstract class BaseExpressionsBuilder<TEntity> where TEntity : class
+{
+    protected record ExpressionPackage(ConstantExpression Constant, ParameterExpression Param, MemberExpression Value);
+
+    private const string ParamMarker = "p";
+    protected virtual MethodInfo? GetContainsMethod<TValue>() => typeof(ICollection<TValue>).GetMethod(nameof(Enumerable.Contains), new[] { typeof(TValue) });
+    protected virtual MethodInfo? GetNullableContainsMethod<TValue>() => typeof(ICollection<TValue>).GetMethod(nameof(Enumerable.Contains), new[] { typeof(TValue?) });
+    protected static MemberExpression GetProperty(Expression param, string target) => Expression.PropertyOrField(param, target);
+    protected static ParameterExpression GetParameter() => Expression.Parameter(typeof(TEntity), ParamMarker);
+    protected static ConstantExpression GetConstant(object source) => Expression.Constant(source);
+    protected static ConstantExpression GetNullableConstant<TValue>(object source) => Expression.Constant(source, typeof(TValue?));
+    protected static ExpressionPackage GetPackage(object constSource, string target)
+    {
+        var constant = GetConstant(constSource);
+
+        var param = GetParameter();
+        { }
+        var property = GetProperty(param, target);
+        { }
+        return new ExpressionPackage(constant, param, property);
+    }
+    protected static ExpressionPackage GetNullablePackage<TValue>(object constSource, string target)
+    {
+        var constant = GetNullableConstant<TValue>(constSource);
+        { }
+        var param = GetParameter();
+        { }
+        var property = GetProperty(param, target);
+        { }
+        return new ExpressionPackage(constant, param, property);
+    }
+}
+
+
+
+
---
+++ CalistoDbCore/Expressions/Builders/Queries/StudentsQueries.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+using CalistoDbCore.Expressions.BuildingOptions;
+using CalistoDbCore.Expressions.Enumerations;
+using CalistoDbCore.Expressions.Extensions;
+using CalistoDbCore.U3FEntities;
+
+using CalistoStandars.Definitions.Interfaces.DbCore.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CalistoDbCore.Expressions.Builders;
+
+
+
+public sealed class StudentsQueries<TEntity> : QueryExpressionsBuilder<TEntity> where TEntity : class, IStudentEntity
+{
+    public StudentsQueries(BuilderOptions options) : base(options) { }
+
+    public IQueryable<TEntity>? GetSyncCareerQuery(IQueryable<TEntity> query)
+    {
+        Expression[] expressions = GetExpressionsOptions(OptionType.Period, 
+            OptionType.Regular, OptionType.ConvCod, OptionType.Career).ToArray()!;
+
+        return expressions.Length == 0 ? null :
+            query.WithExpressions(expressions).AsNoTracking();
+    }
+}
+
+
+
+
